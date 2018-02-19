0de0699e2e824f0f4c6c9c65bf70fa5a7fdc9a0d
Sofiya <sofiya.belyaeva.2013@mail.ru>
2018-02-19 08:55:16 +0300

    Create admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,11 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+**/
+
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['auth'] ], function () {
+   Route::get('/', 'DashboardController@dashboard')->name('admin.index');
+});
 
 Route::get('/', function () {
     return view('welcome');
