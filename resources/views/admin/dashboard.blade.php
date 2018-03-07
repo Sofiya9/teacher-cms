2b30d9f4dfddcdfcb2eaa917cff69d131f7c79f7
Sofiya <sofiya.belyaeva.2013@mail.ru>
2018-03-07 11:43:40 +0300

    Admin dashboard: menu; article, category: list and count

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,16 +1,17 @@
 @extends('admin.layouts.app_admin')
 
 @section('content')
-     <div class="container">
+
+    <div class="container">
       <div class="row">
         <div class="col-sm-3">
           <div class="jumbotron text-center">
-            <p><span class="label label-primary">Категорий 0</span></p>
+            <p><span class="label label-primary">Категорий {{$count_categories}}</span></p>
           </div>
         </div>
         <div class="col-sm-3">
           <div class="jumbotron text-center">
-            <p><span class="label label-primary">Материалов 0</span></p>
+            <p><span class="label label-primary">Записей {{$count_articles}}</span></p>
           </div>
         </div>
         <div class="col-sm-3">
@@ -25,22 +26,31 @@
         </div>
       </div>
 
-      <div class="col-sm-6">
-   <a class="btn btn-block btn-default" href="{{route('admin.article.create')}}">Создать запись блога</a>
-   <a href="#" class="list-group-item">
-     <h4 class="list-group-item-heading">Материал первый</h4>
-     <p class="list-group-item-text">Категория</p>
-   </a>
- </div>
-
-
-            <div class="col-sm-6">
-          <a class="btn btn-block btn-default" href="#">Создать Материал</a>
-         <a class="btn btn-block btn-default" href="{{route('admin.article.create')}}">Создать запись блога</a>
-        <a href="#" class="list-group-item">
-            <h4 class="list-group-item-heading">Материал первый</h4>
-        <p class="list-group-item-text">Категория</p>
+      <div class="row">
+        <div class="col-sm-6">
+          <a class="btn btn-block btn-default" href="{{route('admin.category.create')}}">Создать категорию</a>
+          @foreach ($categories as $category)
+            <a class="list-group-item" href="{{route('admin.category.edit', $category)}}">
+              <h4 class="list-group-item-heading">{{$category->title}}</h4>
+              <p class="list-group-item-text">
+                {{$category->articles()->count()}}
+              </p>
+            </a>
+          @endforeach
+        </div>
+        <div class="col-sm-6">
+          <a class="btn btn-block btn-default" href="{{route('admin.article.create')}}">Создать запись блога</a>
+          @foreach ($articles as $article)
+            <a class="list-group-item" href="{{route('admin.article.edit', $article)}}">
+              <h4 class="list-group-item-heading">{{$article->title}}</h4>
+              <p class="list-group-item-text">
+                {{$article->categories()->pluck('title')->implode(', ')}}
+              </p>
+            </a>
+          @endforeach
+        </div>
       </div>
     </div>
 
-    @endsection
+
+@endsection
