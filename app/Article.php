354dee5167b488f234f10c273af298516bbe1039
Sofiya <sofiya.belyaeva.2013@mail.ru>
2018-03-13 10:04:56 +0300

    Create an article search

--- app/Article.php
+++ app/Article.php
@@ -1,18 +1,13 @@
 <?php
-
 namespace App;
-
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
-
 class Article extends Model
 {
-    //
     // Mass assigned
     protected $fillable = ['title', 'slug', 'description_short', 'description',
                           'image', 'image_show', 'meta_title', 'meta_description',
                            'meta_keyword', 'published', 'created_by', 'modified_by'];
-
     // Mutators
     public function setSlugAttribute($value)
     {
@@ -20,7 +15,6 @@ class Article extends Model
                                     . "-"
                                     . \Carbon\Carbon::now()->format('dmyHi'), '-');
     }
-
     // Polymorphic relation with categories
     public function categories()
     {
@@ -30,5 +24,15 @@ class Article extends Model
     {
         return $query->orderBy('created_at', 'desc')->take($count)->get();
     }
-
+    /**
+     * Поиск по записям блога
+     */
+    public function scopeSearch($query, $search)
+    {
+        return $query->where('title', 'like', '%'.$search.'%')
+                      ->orWhere('description_short', 'like', '%'.$search.'%')
+                      ->orWhere('description', 'like', '%'.$search.'%')
+                      ->orWhere('meta_description', 'like', '%'.$search.'%')
+                      ->orWhere('meta_keyword', 'like', '%'.$search.'%');
+    }
 }
